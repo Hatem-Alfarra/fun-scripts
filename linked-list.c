3fd1a4e602a9c04d88f41bf27a1d469724e78f5b
Hatem Alfarra <halfarra@outlook.com>
2026-01-11 22:54:57 -0500

    list expands with user input as values

--- linked-list.c
+++ linked-list.c
@@ -9,45 +9,44 @@ struct Node
 	struct Node* next;
 };
 
-struct Node* extendNode(struct Node* node, int size){
-	
-	int value = node->value;
-	struct Node *pHEAD = node;
+struct Node* extendNode(struct Node* node, int newValue)
+{	
+	struct Node *pNewNode = malloc(sizeof(struct Node));
+	pNewNode->value = newValue;
+	pNewNode->next = node;
 
-	while(size--){
-		value++;
-		struct Node *pNewNode = malloc(sizeof(struct Node));
-		pNewNode->value = value;
-		pNewNode->next = node;
-		pHEAD = pNewNode;
-		node = pNewNode;
-	}
-	return pHEAD;
+	return pNewNode;
 }
 
 
-void printLinkedList(struct Node* node){
-	
-	printf(" %d ", (int) node->value);
+void printLinkedList(struct Node* node)
+{	
 	while (node->next != NULL) {
+		printf(" %d ->", (int) node->value);
 		node = node->next;
-		printf("-> %d ", (int) node->value);
 	}
+	printf(" NULL");
 }
 
 
-int main(){
-	printf("Enter size of desired linked list:\n");
-	int extend_by;
-	scanf("%d", &extend_by);
+int main()
+{
 	struct Node *pTAIL = malloc(sizeof(struct Node));
 	pTAIL->value = 0;
 	pTAIL->next = NULL;
 
-	struct Node *pHEAD = extendNode(pTAIL, extend_by);	
-	
-	printLinkedList(pHEAD);
+	struct Node *pHEAD = pTAIL;
 
+	int valid_input = 1;	
+	while(valid_input){
+		printf("\nEnter next value in linked list:\n");
+		int newValue;
+		valid_input = scanf("%d", &newValue);
+		if (valid_input){
+			pHEAD = extendNode(pHEAD, newValue);	
+			printLinkedList(pHEAD);
+		}
+	}
 
 	return 0;
 }
