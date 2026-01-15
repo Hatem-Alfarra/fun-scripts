dc85057e97de0b68e241769f4e443053915c20c1
Hatem Alfarra <halfarra@outlook.com>
2026-01-15 03:05:12 -0500

    Abstracting head/top reassigning in methods and cleaning code to adhere to current naming conventions

--- linked-list.c
+++ linked-list.c
@@ -3,27 +3,31 @@
 #include <stdlib.h>
 
 
+
 struct Node
 {
 	int value;
 	struct Node* next;
 };
 
-struct Node* extendNode(struct Node* node, int newValue)
+struct Node* extendNode(struct Node** headRef, int newValue)
 {	
-	struct Node *pNewNode = malloc(sizeof(struct Node));
-	pNewNode->value = newValue;
-	pNewNode->next = node;
+	struct Node *newNode = malloc(sizeof(struct Node));
+	newNode->value = newValue;
+	newNode->next = *headRef;
 
-	return pNewNode;
+	*headRef = newNode;
+
+	return newNode;
 }
 
 
-void printLinkedList(struct Node* node)
+void printLinkedList(struct Node* head)
 {	
-	while (node->next != NULL) {
-		printf(" %d ->", (int) node->value);
-		node = node->next;
+	struct Node *curNode = head;
+	while (curNode->next != NULL) {
+		printf(" %d ->", (int) curNode->value);
+		curNode = curNode->next;
 	}
 	printf(" NULL");
 }
@@ -31,20 +35,20 @@ void printLinkedList(struct Node* node)
 
 int main()
 {
-	struct Node *pTAIL = malloc(sizeof(struct Node));
-	pTAIL->value = 0;
-	pTAIL->next = NULL;
+	struct Node *tail = malloc(sizeof(struct Node));
+	tail->value = 0;
+	tail->next = NULL;
 
-	struct Node *pHEAD = pTAIL;
+	struct Node *head = tail;
 
-	int valid_input = 1;	
-	while(valid_input){
+	int isValid = 1;	
+	while(isValid){
 		printf("\nEnter next value in linked list:\n");
 		int newValue;
-		valid_input = scanf("%d", &newValue);
-		if (valid_input){
-			pHEAD = extendNode(pHEAD, newValue);	
-			printLinkedList(pHEAD);
+		isValid = scanf("%d", &newValue);
+		if (isValid){
+			extendNode(&head, newValue);	
+			printLinkedList(head);
 		}
 	}
 
